a5d0a86399bff4101243e1e425c209b01bae8c0b
johurulhque <johurulhque.cse@gmail.com>
2023-05-20 23:16:29 +0600

    vercel configation

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-  try {
+  // try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const database = client.db("toyHome").collection("toys");
@@ -118,10 +118,10 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+  // } finally {
+  //   // Ensures that the client will close when you finish/error
+  //   // await client.close();
+  // }
 }
 run().catch(console.dir);
 
