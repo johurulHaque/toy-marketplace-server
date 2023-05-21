8fc8f6fc112c1618ef8dad13b4a5e6271d4de4a2
johurulhque <johurulhque.cse@gmail.com>
2023-05-21 14:01:18 +0600

    await in connection is removed and remove the api from try block

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
@@ -9,26 +9,23 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-
 const toys = require("./data/toy.json");
-console.log(process.env.DB_USER)
-console.log(process.env.DB_PASS)
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
 
 // app.get("/toys", (req, res) => {
 //   res.send(toys);
 // });
 
 // app.get('/toy/:id', (req, res) => {
-//     const id = req.params.id; 
+//     const id = req.params.id;
 //     const selectedToy = toys.find(n => n.id == id);
 //     res.send(selectedToy)
 //   })
-  
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
 
-  
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w9tsbcy.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -38,90 +35,85 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   // try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    const database = client.db("toyHome").collection("toys");
+  // Connect the client to the server	(optional starting in v4.7)
+  // await client.connect();
+  client.connect();
+  const database = client.db("toyHome").collection("toys");
 
+  app.get("/toys", async (req, res) => {
+    // const result = await database.find().limit(20).toArray();
+    const result = await database.find().toArray();
+    res.send(result);
+  });
 
-    app.get("/toys", async(req, res) => {
-      const result = await database.find().limit(20).toArray();
-      res.send(result);
-    });
+  app.get("/toy/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await database.findOne(query);
+    res.send(result);
 
+    // const selectedToy = toys.find(n => n.id == id);
+    // res.send(selectedToy)
+  });
 
-    app.get('/toy/:id', async (req, res) => {
-      const id = req.params.id; 
-      const query = { _id: new ObjectId(id) };
-      const result = await database.findOne(query);
-      res.send(result);
+  app.get("/user-toy", async (req, res) => {
+    // const query = req.query;
+    let query = {};
+    if (req.query?.email) {
+      query = { email: req.query.email };
+    }
+    console.log(query);
+    const result = await database.find(query).toArray();
+    res.send(result);
+  });
 
-      // const selectedToy = toys.find(n => n.id == id);
-      // res.send(selectedToy)
-    })
+  app.post("/add-toy", async (req, res) => {
+    const body = req.body;
+    const result = await database.insertOne(body);
+    res.send(result);
+  });
 
-    app.get("/user-toy", async(req, res) => {
-      // const query = req.query;
-      let query = {};
-      if(req.query?.email){
-        query ={email:req.query.email}
-      }
-      console.log(query)
-      const result = await database.find(query).toArray();
-      res.send(result);
-    }); 
+  app.put("/update-toy/:id", async (req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+    const filter = { _id: new ObjectId(id) };
+    const options = { upsert: true };
 
+    const updateDoc = {
+      $set: {
+        name: body.name,
+        email: body.email,
+        image: body.image,
+        price: body.price,
+        quantity: body.quantity,
+        sellerName: body.sellerName,
+        subcategory: body.subcategory,
+      },
+    };
+    const result = await database.updateOne(filter, updateDoc, options);
+    res.send(result);
+  });
 
-    app.post('/add-toy',async(req, res)=>{
-      const body = req.body;
-      const result = await database.insertOne(body);
-      res.send(result);
-    })
+  app.delete("/toy/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await database.deleteOne(query);
+    res.send(result);
+  });
 
-    app.put('/update-toy/:id',async(req, res)=>{
-      const id = req.params.id;
-      const body = req.body;
-      const filter = { _id: new ObjectId(id) };
-      const options = { upsert: true };
+  // const result = await database.insertOne(doc);
 
-      const updateDoc = {
-        $set: {
-          name: body.name,
-          email: body.email,
-          image: body.image,
-          price: body.price,
-          quantity: body.quantity,
-          sellerName: body.sellerName,
-          subcategory: body.subcategory,
-
-        },
-      };
-      const result = await database.updateOne(filter, updateDoc, options);
-      res.send(result);
-    })
-
-    app.delete('/toy/:id',async(req, res)=>{
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await database.deleteOne(query);
-      res.send(result)  
-    })
-
-
-
-    // const result = await database.insertOne(doc);
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  // Send a ping to confirm a successful connection
+  await client.db("admin").command({ ping: 1 });
+  console.log("Pinged your deployment. You successfully connected to MongoDB!");
   // } finally {
   //   // Ensures that the client will close when you finish/error
   //   // await client.close();
   // }
 }
 run().catch(console.dir);
-
